8570f6da70f7182d4ac36caff9db3219718f35e6
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-08-19 12:21:53 +0600

    update triangle section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 /* triangle section */
 document.getElementById("tri-btn").addEventListener("click", function () {
   const inputFieldB = document.getElementById("tri-input-b").value;
-  const inputFieldStringB = parseInt(inputFieldB);
+  const inputFieldStringB = parseFloat(inputFieldB);
   const inputFieldH = document.getElementById("tri-input-h").value;
-  const inputFieldStringH = parseInt(inputFieldH);
-  const result = inputFieldStringB * inputFieldStringH;
-  console.log(result);
+  const inputFieldStringH = parseFloat(inputFieldH);
+  const result = 0.5 * inputFieldStringB * inputFieldStringH;
+  const areaCal = document.getElementById("area-cal");
+  areaCal.innerText = result;
 });
